db4fb1eca1fa4d8fcb0beebce96224a772111ff4
creatordev <utsavmajhi@gmail.com>
2020-03-26 01:19:42 +0530

    added ui components to scramblepage

--- app/src/main/java/com/example/danyal/bluetoothhc05/homepage.java
+++ app/src/main/java/com/example/danyal/bluetoothhc05/homepage.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import pl.bclogic.pulsator4droid.library.PulsatorLayout;
+
 public class homepage extends AppCompatActivity {
     String address = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
+        PulsatorLayout pulsator = (PulsatorLayout) findViewById(R.id.pulsator);
+        pulsator.start();
         //pass the address of connected device(HC05 particular module at a time)
         Intent newint = getIntent();
         address = newint.getStringExtra(DeviceList.EXTRA_ADDRESS);
